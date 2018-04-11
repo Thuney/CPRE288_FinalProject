9195e481533c843818a82da6df6b9962e7984be4
Aaron Thune <aaronthune@gmail.com>
2018-04-11 15:07:39 -0500

    Refactored servo.c and servo.h to be a more useful library

--- servo.c
+++ servo.c
@@ -2,33 +2,23 @@
  *
  * 	@file  servo.c
  *
- *
- *
- *
- *
  *   @author Jonathan Novak
  *
  */
 
 #include "servo.h"
-#include "timer.h"
-#include "button.h"
-#include <stdbool.h>
-#include "driverlib/interrupt.h"
-#include <math.h>
 
-//Calibration
-int max = 37300;
-int mid = 23031;
-int min = 8135;
-double a = 0.006172;
-double b = -50.20922;
+//Calibrated values
+int max = 37300;	 	//Match value at theta = 180 degrees
+int mid = 23031;	 	//Match value at theta = 90 degrees
+int min = 8135;		 	//Match value at theta = 0 degrees
+double msPerDegree = 0.006172; 	//Approximately (2.25-0.75)/180 (based on datasheet for servo)
 
-//stores current count and degrees
+//Store current count and degrees
 double degrees = 0;
-int count = 0;
+int count = 12000; //Theta = 0 degrees
 
-int upordown = 1;
+int upOrDown = 1; //1 if up, -1 if down
 
 void servo_init()
 {
@@ -76,116 +66,55 @@ void servo_init()
 	
 	//0x4E200 = 320,000 (base 10)
     TIMER1_TBILR_R = 0xFA00; //Load value of 64,000 into our interval load register
-    TIMER1_TBPR_R = 0x4;    //Prescaler of 4
+    TIMER1_TBPR_R = 0x4;     //Prescaler of 4
 	
 	//Move servo to default position
-    move_servo(mid);
+    set_servo_pos(90);
 	
 	//enable Timer1B
     TIMER1_CTL_R |= 0x00000100;
-
 }
 
-double countToDeg(int count)
+int calculateDeltaCount(double delta_deg)
 {
-    return (double) a * count + b;
+    return ((((msPerDegree*delta_deg)/20)*320000)/5);
 }
 
-int degreesToCount(double deg)
-{
-    return (deg - b) / a;;
-}
-/**
- * variables that store the count for each step of a certain degree, 1, 2.5, 5 degrees
- */
-int res1;
-int res25;
-int res5;
+void move_servo(int degree)
+{	
+	//Calculate change in counter value
+	count += calculateDeltaCount(degree)
+	count = (count > max) ? max : (count < min) ? min : count;
 
-/**
- * calculates the count resolution for each step of a certain degree
- */
-void calibrate()
-{
-    res1 = degreesToCount(2) - degreesToCount(1);
-    res25 = degreesToCount(3.5) - degreesToCount(1);
-    res5 = degreesToCount(6) - degreesToCount(1);
-}
+    //Set Match Register Value, adjusting pulse width
+	//(320,000/5) - counter
+    TIMER1_TBMATCHR_R = ((0x4E200 / 5) - counter);
+	//Prescaler of 4
+    TIMER1_TBPMR_R =  = 0x4;
+	
+    //Update current position in degrees
+    degrees += degree;
 
-int lastButton = 0;
-int buttonPressed = 0;
-
-void buttonSwitch(uint8_t button)
-{
-    switch (button)
-    {
-    case 6:
-        if ((upordown < 0 && count < min) || (upordown >= 0 && count > max))
-        {
-            return;
-        }
-        count += res1 * upordown;
-        move_servo(count);
-        lastButton = 6;
-        break;
-    case 5:
-        if ((upordown < 0 && count < min) || (upordown >= 0 && count > max))
-        {
-            return;
-        }
-        count += res25 * upordown;
-        move_servo(count);
-        lastButton = 5;
-        break;
-    case 4:
-        if ((upordown < 0 && count < min) || (upordown >= 0 && count > max))
-        {
-            return;
-        }
-        count += res5 * upordown;
-        move_servo(count);
-        lastButton = 4;
-        break;
-    case 3:
-        upordown *= -1;
-        lastButton = 3;
-        break;
-    default:
-        lastButton = 0;
-        break;
-    }
-
-    if (upordown == -1) {
-        lcd_printf("count: %d \ndegrees: %d \ndecrementing", count, (int) degrees);
-    } else {
-        lcd_printf("count: %d \ndegrees: %d \nincrementing", count, (int) degrees);
-    }
-}
-
-void move_servo(unsigned counter)
-{
-    //set match value
-    TIMER1_TBMATCHR_R = (0x4E200 - counter) & 0xFFFF;
-    TIMER1_TBPMR_R = (0x4E200 - counter) >> 16;
-    //update current position in count and degrees
-    count = counter;
-    degrees = countToDeg(count);
-
-    if (upordown == -1) {
+    if (upOrDown == -1)
         lcd_printf("%d count \n%d degrees\ndecrementing", count, (int) degrees);
-    } else {
+    else
         lcd_printf("%d count \n%d degrees\nincrementing", count, (int) degrees);
-    }
+	
+	//Wait for servo to move
     timer_waitMillis(300);
 }
 
-//void main(){
-//    lcd_init();
-//    servo_init();
-//    calibrate();
-//    button_init();
-//    while(1){
-//        int button = button_getButton();
-//        buttonSwitch(button);
-//    }
-//}
+void set_servo_pos(unsigned degrees)
+{
+	//Set Match Register Value, adjusting pulse width
+	
+	//(((320,000 - 12,000) / 5) - counter)
+    TIMER1_TBMATCHR_R = (((0x4E200-12000) / 5) - counter);
+	//Prescaler of 4
+    TIMER1_TBPMR_R = 0x4;
+	
+	lcd_printf("Position set to %d degrees", degrees);
+	
+	//Wait for servo to move
+    timer_waitMillis(300);
+}
--- servo.h
+++ servo.h
@@ -14,16 +14,33 @@
 
 #include <inc/tm4c123gh6pm.h>
 
+#include "driverlib/interrupt.h"
+#include <math.h>
 #include "Timer.h"
-#include "lcd.h"
-#include "button.h"
 
-double countToDeg(int count);
-int degreesToCount(double deg);
-void calibrate();
+/*
+ * Initializes servo settings
+ *
+ * Uses:
+ *  GPIO Port B Pin 5 (as output as a PWM mode Timer)
+ *  Timer 1B (for capturing time events)
+ */
 void servo_init();
-void buttonSwitch(uint8_t button);
-void move_servo(unsigned counter);
+
+/*
+ * Convert a change in degree to a change in prescaled timer counts
+ */
+int calculateDeltaCount(double delta_deg);
+
+/*
+ * Move the servo the specified (signed) number of degrees away from its current position
+ */
+void move_servo(int degrees);
+
+/*
+ * Set the servo to a specified (unsigned) degree measurement
+ */
+void set_servo(unsigned degrees);
 
 
-#endif
+#endif /* SERVO_H_ */
